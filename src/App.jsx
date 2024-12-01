9c19ef9d93f06f1a7c66ea4513f39b132fc609ce
Omar Aedo <a20200688@pucp.edu.pe>
2024-12-01 18:30:37 -0500

    React-QueyCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient } from "@tanstack/react-query";
+
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -9,6 +11,8 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+
+new QueryClient();
 
 function App() {
   return (
